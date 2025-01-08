306ad540edf1643881d2598426d4d8224649622f
ElrheaDeSouza <159645254+ElrheaDeSouza@users.noreply.github.com>
2025-01-08 20:31:10 +0530

    Delete SEM V/OS directory

--- SEM V/OS/Expt2 (Bash)/prog4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Problem Statement: Display the Number Table
-echo "Enter the value of n :"
-read n
-for((i=1; i<=10; i++))
-do
-echo $n \* $i = `expr $n \* $i`
-done
--- SEM V/OS/Expt2 (Bash)/prog5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Problem Statement: square of first 20 numbers
-for((i=1; i<=20; i++))
-do
-echo $i\*$i = `expr $i \* $i`
-done
